10b7f196cf2c5fdebf32f6406629158f3eac2208
roberto <roberticosantana.resb@gmail.com>
2023-11-11 19:14:01 -0600

    Assignment 3-2

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -11,41 +11,45 @@ KERNEL_VERSION=v5.1.10
 BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
-CROSS_COMPILE=aarch64-linux-gnu-
+CROSS_COMPILE=aarch64-none-linux-gnu-
+toolchain=/home/roberto/install-lnx/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu
+appdir=$(realpath $(dirname $0))
+export PATH="${toolchain}/bin:$PATH"
 
-if [ $# -lt 1 ]; then
-  echo "Using default directory ${OUTDIR} for output"
+
+if [ $# -lt 1 ]
+then
+	echo "Using default directory ${OUTDIR} for output"
 else
-  OUTDIR=$1
-  echo "Using passed directory ${OUTDIR} for output"
+	OUTDIR=$1
+	echo "Using passed directory ${OUTDIR} for output"
 fi
 
 mkdir -p ${OUTDIR}
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/linux-stable" ]; then
-  # Clone only if the repository does not exist.
-  echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
-  git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
+    #Clone only if the repository does not exist.
+	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
+	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
 fi
-
 if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
-  cd linux-stable
-  echo "Checking out version ${KERNEL_VERSION}"
-  git checkout ${KERNEL_VERSION}
+    cd linux-stable
+    echo "Checking out version ${KERNEL_VERSION}"
+    git checkout ${KERNEL_VERSION}
 
-  # Configure the kernel
-  make ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} defconfig
-
-  # Build the kernel
-  make -j4 ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE}
-  
-  # Copy the generated kernel Image
-  cp arch/arm64/boot/Image ${OUTDIR}
-
+    # TODO: Add your kernel build steps here
+	
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
+    
 fi
 
 echo "Adding the Image in outdir"
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}/Image
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
@@ -56,77 +60,72 @@ then
 fi
 
 # TODO: Create necessary base directories
-# Create necessary base directories
-mkdir -p ${OUTDIR}/rootfs/{bin,dev,etc,home,lib,lib64,proc,sys,tmp,usr,var}
-mkdir -p ${OUTDIR}/rootfs/home/conf
-cd "$OUTDIR"
+mkdir rootfs
+cd rootfs
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/sbin
+mkdir -p var/log
 
+
+cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
 then
 git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
-    # TODO:  Configure busybox
-    # Configure BusyBox
-    make ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    # TODO:  Configure busyboxv/lib
+    make distclean
+    make defconfig
+    
+
 else
+    echo "skipping busybox config"
     cd busybox
 fi
+
 # TODO: Make and install busybox
+echo "about to make and install busybox"
 
-# Build BusyBox
-make -j4 ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE}
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
-# Install BusyBox
-make ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} CONFIG_PREFIX=${OUTDIR}/rootfs install
 cd ${OUTDIR}/rootfs
-
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
-SYSROOT=/home/roberto/Downloads/toolchain/arm-gnu-toolchain-13.2.Rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc
-# TODO: Add library dependencies to rootfs
-cp -L $SYSROOT/lib64/libm.so.* lib64
-cp -L $SYSROOT/lib64/libresolv.so.* lib64
-cp -L $SYSROOT/lib64/libc.so.* lib64
-cp -L $SYSROOT/lib/ld-linux-aarch64.* lib
+# TODO: Add llib/ld-linux-aarch64.so.1ibrary dependencies to rootfs
+SYSROOT=$(${CROSS_COMPILE}gcc -print-sysroot)
+cp ${SYSROOT}/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib
+cp ${SYSROOT}/lib64/libm.so.6 ${OUTDIR}/rootfs/lib64
+cp ${SYSROOT}/lib64/libresolv.so.2 ${OUTDIR}/rootfs/lib64
+cp ${SYSROOT}/lib64/libc.so.6 ${OUTDIR}/rootfs/lib64
+
+
 # TODO: Make device nodes
-# Create device nodes
 sudo mknod -m 666 ${OUTDIR}/rootfs/dev/null c 1 3
-sudo mknod -m 600 dev/console c 5 1 
+sudo mknod -m 600 ${OUTDIR}/rootfs/dev/console c 5 1
 
 # TODO: Clean and build the writer utility
-# Compile the writer utility and copy it to "${OUTDIR}/rootfs/home"
-cd $FINDER_APP_DIR 
+cd ${appdir}
 make clean
-make CROSS_COMPILE=${CROSS_COMPILE}
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
 
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
-cp $FINDER_APP_DIR/finder-test.sh ${OUTDIR}/rootfs/home
-cp $FINDER_APP_DIR/conf/ -r ${OUTDIR}/rootfs/home
-cp $FINDER_APP_DIR/Makefile ${OUTDIR}/rootfs/home
-cp $FINDER_APP_DIR/writer ${OUTDIR}/rootfs/home
-cp $FINDER_APP_DIR/writer ${OUTDIR}/rootfs/usr/bin
-cp $FINDER_APP_DIR/autorun-qemu.sh ${OUTDIR}/rootfs/home
+cp -a writer ${OUTDIR}/rootfs/home
+cp -a finder.sh ${OUTDIR}/rootfs/home
+cp -a finder-test.sh ${OUTDIR}/rootfs/home
+cp -a autorun-qemu.sh ${OUTDIR}/rootfs/home
+cp -ra ../conf ${OUTDIR}/rootfs/home/conf
 
-# Copy required files to "${OUTDIR}/rootfs/home"
-cp $FINDER_APP_DIR/finder.sh ${OUTDIR}/rootfs/home
-cp $FINDER_APP_DIR/conf/username.txt ${OUTDIR}/rootfs/home/conf
-#cp $FINDER_APP_DIR/conf/assignment.txt ${OUTDIR}/rootfs/home/conf
 
 # TODO: Chown the root directory
-# Change ownership of "${OUTDIR}/rootfs" to root
 cd ${OUTDIR}/rootfs
-
-sudo chown -R root:root ${OUTDIR}/rootfs
+sudo chown -R root:root *
 
 # TODO: Create initramfs.cpio.gz
-# Create initramfs.cpio.gz
 find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
 cd ${OUTDIR}
-
-rm -f initramfs.cpio.gz
-gzip initramfs.cpio 
+gzip -f initramfs.cpio
