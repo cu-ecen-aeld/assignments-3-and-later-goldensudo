037d1398226e7057a06bf7c629181b55c3957455
roberto <roberticosantana.resb@gmail.com>
2023-11-16 18:37:48 -0600

    modifying

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -1,27 +1,17 @@
-#!/bin/sh
+#! /bin/sh
 
-# Set the path to your aesdsocket executable
-AESDSOCKET_BIN="/path/to/your/aesdsocket"
-
-case "$1" in
-  start)
-    echo "Starting aesdsocket daemon..."
-    start-stop-daemon --start --background --make-pidfile --pidfile /var/run/aesdsocket.pid \
-      --exec "$AESDSOCKET_BIN" -d
-    ;;
-  stop)
-    echo "Stopping aesdsocket daemon..."
-    start-stop-daemon --stop --pidfile /var/run/aesdsocket.pid
-    ;;
-  restart)
-    $0 stop
-    sleep 1
-    $0 start
-    ;;
-  *)
-    echo "Usage: $0 {start|stop|restart}"
-    exit 1
-    ;;
+    case "$1" in
+      start)
+        echo "aesdsocket started"
+        start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket -- -d
+        ;;
+      stop)
+        echo "Stopping aesdsocket daemon"
+        start-stop-daemon -K -n aesdsocket
+        ;;
+      *)
+        echo "Usage: $0 {start|stop}"
+        exit 1
 esac
 
-exit 0
+exit O
